055b1ff3ad434d1cf7c6987cb4f67c2f5c711776
rgs-234 <rachelgsnyder@gmail.com>
2024-06-06 18:48:52 -0500

    complete recusive/non-recusive solutions to fibbonacci assignment

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,22 @@
-def fibonacci(n):
-  pass
 
+# Recursive solution
+# def fibonacci(n):
+#   if n <= 1:
+#     return n
+#   else:
+#     return fibonacci(n - 1) + fibonacci(n - 2)
+    
+
+
+# Non-recursive solution
+def fibonacci(n):
+  num1 = 0
+  num2 = 1
+
+  for num in range(n):
+    temp = num2
+    num2 = num1 + num2
+    num1 = temp
+
+  return num1
 
